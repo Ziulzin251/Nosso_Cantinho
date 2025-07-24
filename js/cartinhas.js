c94bdc68c53f96aa85bb55cadcfdc23563f4d52b
DevLuiz271 <144502476+Ziulzin251@users.noreply.github.com>
2025-07-24 00:34:05 -0300

    Issues Cartinhas
    Script >> Random Msg Begin
    style >> Design msg
    Style >> Media Querie

--- js/cartinhas.js
+++ js/cartinhas.js
@@ -4,6 +4,8 @@ function randomMsg(number){
     return msgs[number]
 }
 
+msg.innerHTML = randomMsg(Math.floor(Math.random() * 100))
+
 document.body.addEventListener("click", () => {
     
     msg.innerHTML = randomMsg(Math.floor(Math.random() * 100))
